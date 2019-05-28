8205cfb4d42a8f600f09919d93cc3a30e978821f
Nikolaeva Olga Valerevna <ovnikolaeva@stud.kpfu.ru>
2019-05-28 13:46:19 +0300

    добавлено распределение ролей

---
+++ 11-708-exam/Exam2/Exam2/Models/Basket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public class Basket
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public List<string> DishesId { get; private set; }
+        public int Cost { get; set; }
+        public void AddDish(Dishe dish)
+        {
+            DishesId.Add(dish.Id);
+            Cost += dish.Cost;
+        }
+    }
+}
---
+++ 11-708-exam/Exam2/Exam2/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public class Order
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+
+        public List<string> DishesId { get; set; }
+
+        public int Cost { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
